7b3e0005cef73167b03c69ec6d2edcd3bacd4cf0
Joe <ll15j3lm@leeds.ac.uk>
2020-03-27 18:16:00 +0000

    Mark a border (exit point)

--- Python/Numpy_plotting.py
+++ Python/Numpy_plotting.py
@@ -21,3 +21,11 @@ randomY = (inputsize/2) + random.randint(0,1)
 randomY = math.ceil(randomY)
 
 matrix[randomX][randomY] = 1
+
+    # mark a border 
+    for i in range(inputsize - 1):
+        matrix[0][i] = 2
+        matrix[inputsize - 1][i] = 2
+        matrix[i][0] = 2
+        matrix[i][inputsize - 1] = 2
+        matrix[inputsize - 1][inputsize - 1] = 2
